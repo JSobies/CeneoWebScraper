bb725613c233e30b66e657eaee5fee3bc19f4c9c
JSobies <15015@eans-nt.edu.pl>
2025-04-29 11:13:55 +0200

    new functions

--- app/views.py
+++ app/views.py
@@ -1,12 +1,22 @@
 from app import app
-from flask import render_template
+from flask import render_template,request,redirect, url_for
 
 @app.route('/')
 def index(): 
     return render_template("base.html")
 
-@app.route('/extract')
-def extract(): 
+@app.route('/extract', methods=['GET', 'POST'])
+def extract():
+    product_id = request.form.get('product_id')
+    if request.method == 'POST':
+        url = request.form['product_url']
+        num = request.form.get('num_opinions', 50)
+        rating = request.form.get('rating_filter', '')
+
+        
+        print(f"URL: {url}, Opinie: {num}, Ocena: {rating}")
+        return redirect(url_for('product', product_id=product_id))
+
     return render_template("extract.html")
 
 @app.route('/products')
