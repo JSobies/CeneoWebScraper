9d127e42af28b971da9289ce409747385913aea0
JSobies <15015@eans-nt.edu.pl>
2025-05-13 10:52:24 +0200

    single file template

--- app/views.py
+++ app/views.py
@@ -1,23 +1,27 @@
 import os
+import pandas as pd 
 from app import app
 from flask import render_template,request,redirect, url_for,json
 
+
+def list_to_html(l):
+    str_repr = "<ul>"
+    for element in l:
+        str_repr += f"<li>{element}<li/>"
+    str_repr += "<ul>"
+    return "<ul>"+"".join([])+"<ul/>"
+
 @app.route('/')
 def index(): 
-    return render_template("base.html")
+    return render_template("index.html")
 
-@app.route('/extract', methods=['GET', 'POST'])
-def extract():
+@app.route('/extract', methods=['post'])
+def extract_data():
     product_id = request.form.get('product_id')
-    if request.method == 'POST':
-        url = request.form['product_url']
-        num = request.form.get('num_opinions', 50)
-        rating = request.form.get('rating_filter', '')
+    return redirect(url_for('product', product_id=product_id))
 
-        
-        print(f"URL: {url}, Opinie: {num}, Ocena: {rating}")
-        return redirect(url_for('product', product_id=product_id))
-
+@app.route('/extract', methods=['get'])
+def display_form():
     return render_template("extract.html")
 
 @app.route('/products')
@@ -35,6 +39,17 @@ def author():
     return render_template("author.html")
 
 @app.route('/product/<int:product_id>')
-def product(product_id): 
-    return render_template("product.html", product_id=product_id)
+def product(product_id:int): 
+    with open(f"./app/data/opinions/{product_id}.json", "r", encoding="UTF-8") as jf:
+        try:
+            opinions = json.load(jf)
+        except json.JSONDecodeError:
+            error = "Dla produktu o podanym ID nie pobrano jeszcze opinii."
+            return render_template("product.html", error=error)
+    opinions = pd.DataFrame.from_dict(opinions)
+    opinions.pros = opinions.pros.apply(list_to_html)
+    opinions.cons = opinions.pros.apply(list_to_html)
+
+    
+    return render_template("product.html", opinions=opinions.to_html(classes="table table-hover table-bordered"))
 
